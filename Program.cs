65e6c4a62f9496ff905d3137605e7f4b41f9b30f
Malek Mustafa <malekmustafa@Mac.Dlink>
2025-02-02 19:15:59 +0100

    Startade dev-api branchen, skapat första utkast av API:et samt AWS-setup

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!!!");
+app.MapGet("/api/kryptering/kryptera", (string text) => 
+{
+    if (string.IsNullOrEmpty(text)) return Results.BadRequest("Text kan inte vara tomt.");
+    
+    string encrypted = new string(text.Select(c => char.IsLetter(c) 
+        ? (char)(c + 3) : c).ToArray());
+
+    return Results.Ok(encrypted);
+});
+
+app.MapGet("/api/kryptering/avkryptera", (string text) => 
+{
+    if (string.IsNullOrEmpty(text)) return Results.BadRequest("Text kan inte vara tomt.");
+    
+    string decrypted = new string(text.Select(c => char.IsLetter(c) 
+        ? (char)(c - 3) : c).ToArray());
+
+    return Results.Ok(decrypted);
+});
 
 app.Run();
